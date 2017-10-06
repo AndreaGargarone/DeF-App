565fd6b0e9800935635df8bea00e2302ac282e48
Andrea Gargarone <CapacitorGreen123>
2017-10-06 23:39:45 +0200

    Prova commit per Alessio

--- app/src/main/java/it/aea_digital/df/MainActivity.java
+++ app/src/main/java/it/aea_digital/df/MainActivity.java
@@ -1,18 +1,29 @@
 package it.aea_digital.df;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.StrictMode;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import Connection.Connection;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // nascondere action bar
+        //getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
         //commento di prova
         //HttpPostActivity httpInstance = new HttpPostActivity();
         TextView myText = (TextView) findViewById(R.id.myText);
@@ -25,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
         myText.setText(conn.connect());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_rincipale, menu);
+        return true;
     }
 }
